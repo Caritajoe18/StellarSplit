bfc99470c39dc16c0d3fff41dde9b5f9990f1334
Sani Rich Salifu <salifusanirich@gmail.com>
2026-01-22 00:17:12 +0100

    chore(frontend): Configure Tailwind CSS for the frontend setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="max-w-lg mx-auto p-4">
+      <div className="flex justify-center space-x-8 mb-8">
+        <img src={"/vite.svg"} className="h-12 mb-4" alt="Vite logo" />
+        <img src={reactLogo} className="h-12 mb-4" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="text-center">
+        Initialized with Vite, React, TypeScript and Tailwind CSS
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
